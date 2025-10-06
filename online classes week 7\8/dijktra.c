dcd7b280f77dd8f5f1a2e6255dad52b495f9e7c9
Ojasvi <ojasvi.gupta170504@gmail.com>
2025-10-06 21:52:47 +0530

    Create dijktra.c

---
+++ online classes week 7\8/dijktra.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define INF 9999
+#define V 5
+
+int minDistance(int dist[], int sptSet[]) {
+    int min = INF, min_index;
+    for (int v = 0; v < V; v++)
+        if (!sptSet[v] && dist[v] <= min) {
+            min = dist[v];
+            min_index = v;
+        }
+    return min_index;
+}
+
+void dijkstra(int graph[V][V], int src) {
+    int dist[V];
+    int sptSet[V] = {0};
+
+    for (int i = 0; i < V; i++)
+        dist[i] = INF;
+    dist[src] = 0;
+
+    for (int count = 0; count < V-1; count++) {
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = 1;
+
+        for (int v = 0; v < V; v++)
+            if (!sptSet[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+
+    printf("Vertex\tDistance from Source\n");
+    for (int i = 0; i < V; i++)
+        printf("%d\t%d\n", i, dist[i]);
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 10, 0, 0, 5},
+        {0, 0, 1, 0, 2},
+        {0, 0, 0, 4, 0},
+        {7, 0, 6, 0, 0},
+        {0, 3, 9, 2, 0}
+    };
+
+    dijkstra(graph, 0);
+    return 0;
+}
